ec79218ee9161dda29d2937dc66f7f393a1b2f58
JRGreco <84739780+JRGreco@users.noreply.github.com>
2023-10-30 14:10:18 +0100

    Create ruleta.sh

---
+++ ruleta.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Colors
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+function ctrl_c(){
+  echo -e "\n\n${redColour}[!] Saliendo...${endColour}\n"
+  tput cnorm; exit 1
+}
+
+#Ctrl+C
+trap ctrl_c INT
+
+function helpPanel(){
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso...${endColour}${purpleColour} $0${endColour}\n"
+  echo -e "\t${blueColour}-m)${endColour}${grayColour} Dinero con el que desea jugar${endColour}"
+  echo -e "\t${blueColour}-t)${endColour}${grayColour} Técnica a utilizar${endColour}${purpleColour} (${endColour}${yellowColour}martingala${endColour}${blueColour}/${endColour}${yellowColour}inverseLabrouchere${endColour}${purpleColour})${endColour}\n"
+  exit 1
+}
+
+function martingala(){
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Dinero actual: ${endColour}${yellowColour}$money€${endColour}\n"
+  echo -ne "${yellowColour}[+]${endColour}${grayColour} ¿Cuánto dinero tienes pensado apostar? --> ${endColour}" && read initial_bet
+  echo -ne "${yellowColour}[+]${endColour}${grayColour} ¿A qué deseas apostar continuamente (par/impar)? --> ${endColour}" && read par_impar
+
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Vamos a jugar con una cantidad inicial de${endColour}${yellowColour} $initial_bet€${endColour}${grayColour} a ${endColour}${yellowColour}$par_impar${endColour}"
+
+  tput civis # Ocultar el cursor
+  while true; do
+    money=$(($money-$initial_bet))
+    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Acabas de apostar${endColour}${yellowColour} $initial_bet€${endColour}${grayColour} y tienes${endColour}${yellowColour} $money€${endColour}"
+    random_number="$(($RANDOM % 37))"
+    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Ha salido el número ${endColour}${blueColour}$random_number${endColour}"
+
+    if [ "$par_impar" == "par" ]; then
+      if [ "$(($random_number % 2))" -eq 0 ]; then
+        if [ "$random_number" -eq 0 ]; then
+          echo -e "${yellowColour}[+]${endColour}${redColour} Ha salido el 0, por tanto perdemos${endColour}"
+        else
+          echo -e "${yellowColour}[+]${endColour}${greenColour} El número que ha salido es par, ¡ganas!${endColour}"
+          reward=$(($initial_bet*2))
+          echo -e "${yellowColour}[+]${endColour}${grayColour} Ganas un total de ${endColour}${yellowColour}$reward€${endColour}"
+          money=$(($money+$reward))
+          echo -e "${yellowColour}[+]${endColour}${grayColour} Tienes ${endCOlour}${yellowColour}$money€${endColour}"
+        fi
+      else
+        echo -e "${yellowColour}[-]${endColour}${redColour} El número que ha salido es impar, ¡pierdes!${endColour}"
+      fi
+    sleep 0.4
+  fi
+  done
+
+  tput cnorm # Recuperamos el cursor
+}
+
+while getopts "m:t:h" arg; do
+  case $arg in
+    m) money=$OPTARG;;
+    t) technique=$OPTARG;;
+    h) helpPanel;;
+  esac
+done
+
+if [ $money ] && [ $technique ]; then
+  if [ "$technique" == "martingala" ]; then
+    martingala
+  else
+    echo -e "\n${redColour}[!] La técnica introducida no existe${endColour}"
+    helpPanel
+  fi
+else 
+  helpPanel
+fi
